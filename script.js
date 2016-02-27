991e5aae4d42798969438d85170d590b3f14d1a2
leviathananthony <leviwalsh13@gmail.com>
2016-02-27 14:41:08 -0800

    Create script.js
    Clutch

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("levi");
+
+
+ function startingNumber() {
+          var firstNumber = document.getElementById("randomNumberPlacement");
+          firstNumber.innerHTML = Math.floor((Math.random() * 14) + 1);
+          makeOpArray();
+
+}
+
+function makeOpArray() {
+	//console.log("levi has a lame computer");
+	var opArray = document.getElementById("opArray")[];
+	opArray.innerHTML = "dope";
+}
