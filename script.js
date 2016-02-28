ef791bfa750893ea3ac15c2b32f9b8598dcb3106
leviathananthony <leviwalsh13@gmail.com>
2016-02-28 10:57:09 -0800

    Update script.js
    no more freezing

--- script.js
+++ script.js
@@ -65,45 +65,46 @@ $(document).ready(function() {
 		for (i = 1; i < 5; i++) {
 			//var diviseRandom;
 			determiningOperator = (Math.floor(Math.random() * 3) + 1);
-			//console.log(determiningOperator);
 	    	//Addition
 	    	if (determiningOperator === 1) {
-		    	addRandom = Math.floor(Math.random() * 15) + 1;
-		    	while (runningTotal + addRandom > 400) {
+		    	 addRandom = Math.floor(Math.random() * 15) + 1;
+		    	/*while (runningTotal + addRandom > 400) {
 					addRandom = Math.floor(Math.random() * 15) + 1;
-	        	}
+	        	}*/
+	        	/*do {
+	        		addRandom = Math.floor(Math.random() * 15) + 1;
+	        	} while (runningTotal + addRandom > 400);*/
 		    	addToPage(i, "+ " + addRandom);
 		    	//To show js not to concatenate
 		    	runningTotal = (runningTotal / 1) + addRandom;
 			//Multiplication
   			} else if (determiningOperator === 2) {
 	    		multiplyRandom = (Math.floor(Math.random() * 15) + 1);
-	    		while (runningTotal * multiplyRandom > 400 ||
+	    		/* while (runningTotal * multiplyRandom > 400 ||
 	    				multiplyRandom == 1) {
 	    			multiplyRandom = (Math.floor(Math.random() * 15) + 1);
 	    		}
+	    		do {
+	    			multiplyRandom = (Math.floor(Math.random() * 15) + 1);
+	    		
+	    		} while (runningTotal * multiplyRandom > 400 ||
+	    				multiplyRandom == 1);*/
 	    		addToPage(i, "x " + multiplyRandom);
 	    		runningTotal *= multiplyRandom;
 	    	//Subtraction
 	    	} else {
 	    		subtractRandom = (Math.floor(Math.random() * 15) + 1);
-	    		while (runningTotal - subtractRandom <= 0) {
+	    		/*while (runningTotal - subtractRandom <= 0) {
 	    			subtractRandom = (Math.floor(Math.random() * 15) + 1);
 	    		}
+	    		do {
+	    			subtractRandom = (Math.floor(Math.random() * 15) + 1);
+	    		} while (runningTotal - subtractRandom <= 0);*/
+
 	    		addToPage(i, "- " + subtractRandom);
 	    		runningTotal -= subtractRandom;
 	    	}
-	    	//Division
-	    	/*
-	    	} else {
-	    		diviseRandom = Math.floor(Math.random() * 20) + 1;	
-	    		while (runningTotal % diviseRandom != 0) {
-	    			diviseRandom = Math.floor(Math.random() * 20) + 1;
-	    		}
-	    		addToPage(i, "Divided by " + diviseRandom + "\n");
-	    		runningTotal /= diviseRandom;
-	    	}
-	    	*/
+	 
 	    }
 	    finalAnswer = runningTotal;
 	    //alert(runningTotal);
@@ -113,11 +114,6 @@ $(document).ready(function() {
 		//$("highscore").innerHTML = score;
 		$("#highscore").html(score);
 	}
-/**
-	function printTotal(currTotal) {
-		alert(currTotal);
-	}
-**/
     function addToPage(loopNum, element) {
     	if (loopNum === 1) {
     		$("#one").append(element);
@@ -128,54 +124,5 @@ $(document).ready(function() {
     	} else {
     		$("#four").append(element);    		
     	} 
-    	/*if (loopNum === 5) {
-    		$("#five").append(element);
-    	} else if (loopNum === 6) {
-    		$("#six").append(element);    		
-    	} else {
-    		$("#seven").append(element);
-    	}
-    	*/
+
     }
-
-/*
-	function makeOpArray() {
-		for (int i = 0; i < 8; i++) {
-			var opChooser = Math.floor((Math.random() * 4) + 1);
-			if (opChooser === 0) {
-				opArray[i] = addOp;
-			} else if (opChooser === 1) {
-				opArray[i] = multOp;
-			} else if (opChooser === 2) {
-				opArray[i] = subOp;
-			} else {
-				opArray[i] = divOp;
-			}
-		}
-		for (int i = 0; i < opArray.length; i++) {
-			$("#zero").append(opArray[i]);
-		}
-		//opArray.innerHTML = "dope";
-	}
-
-	function addOp(first, second) {
-		return eval("first + second");
-	}
-
-	function multOp(first second) {
-		return eval("first * second");
-	}
-
-	function subOp(first, second) {
-		return eval("first - second");
-	}
-
-	function divOp(first, second) {
-		return eval("first / second");
-	}
-      
-	function startingNumber() {
-        var startingNumber = (Math.floor(Math.random() * 20) + 1);
-		$("zero").append(startingNumber);
-	}
-*/
